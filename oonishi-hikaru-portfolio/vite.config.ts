f1e3dfd9c106c8b339cac48bb6eb4405288577d4
matoba0710mates <matoba0710@icloud.com>
2025-07-09 15:15:02 +0900

    修正

--- oonishi-hikaru-portfolio/vite.config.ts
+++ oonishi-hikaru-portfolio/vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/oonishi-hikaru-portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/site/' : '/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
 })
